641a78599175b490da63d85a7795f6261f2ea0c5
Noel A <1015895@student.sae.edu.au>
2020-07-20 05:02:44 +1000

    Making the game works
    putting player and making it move, adding animation when touch begin.

---
+++ Treasure Hunter/Assets/Scripts/DragFingerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragFingerMovement : MonoBehaviour
+{
+    public Animator animator;
+
+    private Vector3 touchPosition;
+    private Rigidbody2D rb;
+    private Vector3 direction;
+    private float moveSpeed = 10f;
+
+    bool touchStart = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            touchStart = true;
+
+            if (GetComponent<CapsuleCollider2D>() == Physics2D.OverlapPoint(touchPosition) && touchStart)
+            {
+                animator.SetBool("SpeedNew", true);
+            }
+
+            touchPosition.z = 0;
+            direction = (touchPosition - transform.position);
+
+            Vector3 characterScale = transform.localScale;
+            if (touchPosition.x <= 0)
+            {
+                characterScale.x = -15;
+            }
+            if (touchPosition.x >= 0)
+            {
+                characterScale.x = 15;
+            }
+            transform.localScale = characterScale;
+
+            rb.velocity = new Vector2(direction.x, direction.y) * moveSpeed;
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                rb.velocity = Vector2.zero;
+                animator.SetBool("SpeedNew", false);
+            }
+        }
+    }
+}
---
+++ Treasure Hunter/Assets/Scripts/TouchAnimSampleScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchAnimSampleScript : MonoBehaviour
+{
+
+	// Animator component reference
+	Animator _animator;
+
+	// this variable is needed to control body animation more precisely
+	bool strokingAllowed = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		// Getting game objects animator component
+		_animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// getting a touch and its position from input 
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+			// processing touch phases
+			switch (touch.phase)
+			{
+
+				// when you touch the screen for the first time and if you touches a body collider then animation is allowed
+				// so if you touch another screen point then animation will not start
+				// in another words you have to touch a body to start an animation
+				case TouchPhase.Began:
+					if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+					{
+						strokingAllowed = true;
+					}
+					break;
+
+				// if you move your finger touching a body then animation is playing by setting animators isBodyStroked variable to true
+				case TouchPhase.Moved:
+					if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos) && strokingAllowed)
+					{
+						_animator.SetBool("isBodyStroked", true);
+					}
+
+					// if your finger is off the body then animation stops by setting animators isBodyStroked variable to false
+					if (GetComponent<Collider2D>() != Physics2D.OverlapPoint(touchPos))
+					{
+						_animator.SetBool("isBodyStroked", false);
+					}
+					break;
+
+				// if you stop moving your finger then animation stops by setting animators isBodyStroked variable to false
+				case TouchPhase.Stationary:
+					_animator.SetBool("isBodyStroked", false);
+					break;
+
+				// if you release your finger then animation stops by setting animators isBodyStroked variable to true
+				// and animation is not allowed anymore until you touch a body again
+				case TouchPhase.Ended:
+					_animator.SetBool("isBodyStroked", false);
+					strokingAllowed = false;
+					break;
+			}
+		}
+	}
+}
